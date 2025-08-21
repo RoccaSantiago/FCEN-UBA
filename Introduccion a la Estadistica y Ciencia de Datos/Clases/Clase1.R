1f04c5714e518ea33b00045c66ede276f21bb961
Santiago <Santiagorocca17@gmail.com>
2025-08-21 12:45:27 -0300

    Algunas cosas de la clase 1 de Estadistica

---
+++ Introduccion a la Estadistica y Ciencia de Datos/Clases/Clase1.R
@@ -0,0 +1,22 @@
+# Ejercicio 1
+
+#a
+sum_a <- 0
+
+for (i in 1:1000){
+  sum <- sum + i
+}
+
+print(sum)
+
+#b
+
+i <- 0
+sum_b <- 0
+
+while (sum_b <= 1000){
+  sum_b <- sum_b + i
+  i <- i+1
+}
+
+print(i)
